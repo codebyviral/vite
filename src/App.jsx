56e26129f84b2eb38be8ca2f583672422dfcb589
codebyviral <viralvaghela2641@gmail.com>
2026-04-03 18:24:22 +0530

    add update

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,7 @@ const App = () => {
     <div>
       <h1 className='text-3xl'>Hello World</h1>
       <h2>Welcome to Earth!</h2>
-      <h3>Earth is amazing!!</h3>
-      <h4>4:35 PM</h4>
-      <h5>Thu 2,Apr</h5>
-      <h6>updatesss-----</h6>
+      <h3>Github push working...</h3>
     </div>
   )
 }
