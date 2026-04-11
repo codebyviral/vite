96b87c1cac5c1d62808f7c84d9e2db4ed0d94309
Viral Vaghela <viralvaghela8577@gmail.com>
2026-04-11 22:35:28 +0530

    sueeee

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
     <div>
-      <h1 className='text-3xl'>Hello World</h1>
+      <h1 className="text-3xl">Hello World</h1>
       <h2>Welcome to Earth!</h2>
       <h3>Github push working...</h3>
+      <h4>Project completed 100%.....</h4>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
